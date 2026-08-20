2baba41015cc17ef1e3f30f0200944ad79788542
keryee18 <kychong-wm24@student.tarc.edu.my>
2026-08-20 16:04:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ from sklearn.tree import DecisionTreeClassifier
 
 st.set_page_config(page_title="Obesity Levels Predictor", layout="wide")
 
-DATA_PATH = Path(__file__).parent / "data" / "complete_processed_obesity_dataset_unified.csv"
+DATA_PATH = Path(__file__).parent / "data" / "ObesityDataSet_Processed_Unified_SeparatedSteps.csv"
 TARGET = "NObeyesdad"
 RANDOM_STATE = 42
 
